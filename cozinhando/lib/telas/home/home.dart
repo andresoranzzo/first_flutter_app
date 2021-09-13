62bd15f92244da914b73f961c4b7f4865f8a2f06
andresoranzzo <andresoranzzo@gmail.com>
2021-09-12 23:25:56 -0300

    terminando layout da tela home

--- cozinhando/lib/telas/home/home.dart
+++ cozinhando/lib/telas/home/home.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:cozinhando/modelos/receita.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return _construirHome('Cozinhando em casa', 'Bolo de chocolate');
@@ -8,38 +17,79 @@ class Home extends StatelessWidget {
 
   Widget _construirHome(String titulo, String textoCard) {
     return Scaffold(
-      body: _construirCard(textoCard),
+      body: _construirListaCard(),
       appBar: AppBar(title: Text(titulo)),
     );
   }
 
-  Widget _construirCard(String textoCard) {
+  Widget _construirListaCard() {
+    return FutureBuilder(
+      future: DefaultAssetBundle.of(context).loadString('assets/receitas.json'),
+      builder: (context, snapshot) {
+        List<dynamic> receitas = json.decode(snapshot.data.toString());
+        return ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            Receita receita = Receita.fromJson(receitas[index]);
+            return _construirCard(receita.titulo, receita.foto);
+          },
+          itemCount: receitas == null ? 0 : receitas.length,
+        );
+      },
+    );
+  }
+
+  Widget _construirCard(textoCard, foto) {
     return SizedBox(
-      height: 400,
+      height: 332,
       child: Card(
           margin: EdgeInsets.all(16),
           child: Column(
             children: <Widget>[
-              _construirImagemCard(),
-              _construirTextoCard(textoCard)
+              Stack(
+                children: <Widget>[
+                  _construirImagemCard(foto),
+                  _construirGradienteCard(),
+                  _construirTextoCard(textoCard)
+                ],
+              )
             ],
           )
       ),
     );
   }
 
-  Widget _construirTextoCard(String textoCard) {
+  Widget _construirGradienteCard() {
+    return Container(
+      height: 300,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: FractionalOffset.topCenter,
+          end: FractionalOffset.bottomCenter,
+          colors: [
+            Colors.transparent,
+            Colors.deepOrange.withOpacity(0.7)
+          ]
+        )
+      ),
+    );
+  }
+
+  Widget _construirTextoCard(titulo) {
     return Positioned(
         bottom: 10,
         left: 10,
-        child: Text(
-            textoCard, style: TextStyle(fontSize: 20)));
+        child: Text(titulo, style: TextStyle(
+            fontSize: 20,
+            color: Colors.white
+            )
+        )
+    );
   }
 
-  Widget _construirImagemCard() {
-    return Image.network(
-        'https://blogdaflaviana.com.br/wp-content/uploads/2016/01/Baixar-Imagens-de-Bolo.jpg',
+  Widget _construirImagemCard(foto) {
+    return Image.asset(
+        foto,
         fit: BoxFit.fill,
-        height: 340);
+        height: 300);
   }
 }
